75545fc89f4da133050e05ae768efc7fc91a5b88
kushpatelj86 <111901629+kushpatelj86@users.noreply.github.com>
2023-02-10 17:26:21 -0800

    Delete HellowWorld.cpp

--- HellowWorld.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-int main()
-{
-    cout << "Hello world!" << endl; // print() in python System.out.print() in java
-    return 0; //endl creates new line
-}
